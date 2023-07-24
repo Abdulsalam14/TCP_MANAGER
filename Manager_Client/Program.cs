45d1784bf0b43eac82265b158acf60c88110523a
Abdulsalam Abdullayev <abdullayevabdulsalam@gmail.com>
2023-07-24 23:42:40 +0400

    Add project files.

---
+++ Manager_Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using Manager_Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+var client = new TcpClient();
+client.Connect(ip, port);
+
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+Command command = null;
+string response = null;
+
+
+while (true)
+{
+    Console.WriteLine("Write command or HELP: ");
+    var str = Console.ReadLine().ToUpper();
+    if (str == "HELP")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Command list: ");
+        Console.WriteLine(Command.ProcessList);
+        Console.WriteLine($"{Command.Run} <process_name>");
+        Console.WriteLine($"{Command.Kill} <process_name>");
+        Console.WriteLine("HELP");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+    var input = str.Split(' ');
+    switch (input[0])
+    {
+        case Command.ProcessList:
+            command = new Command { Text = input[0] };
+            bw.Write(JsonSerializer.Serialize(command));
+            response = br.ReadString();
+            var processList = JsonSerializer.Deserialize<string[]>(response);
+            foreach (var processName in processList)
+            {
+                Console.WriteLine($"    {processName}");
+            }
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case Command.Kill:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            response = br.ReadString();
+            Console.WriteLine(response);
+            break;
+        case Command.Run:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            response=br.ReadString();
+            Console.WriteLine(response);
+            break;
+        default:
+            break;
+    }
+}
---
+++ Manager_Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using Manager_Server;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+listener.Start();
+
+
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var br = new BinaryReader(stream);
+    var bw = new BinaryWriter(stream);
+    while (true)
+    {
+        var input = br.ReadString();
+        var command = JsonSerializer.Deserialize<Command>(input);
+
+        if (command is null) continue;
+        Console.WriteLine(command.Text);
+        Console.WriteLine(command.Param);
+        switch (command.Text)
+        {
+            case Command.ProcessList:
+                var processes = Process.GetProcesses();
+                var processesNames = JsonSerializer
+                    .Serialize(processes.Select(p => p.ProcessName));
+                bw.Write(processesNames);
+                break;
+            case Command.Kill:
+                var process=Process.GetProcesses().ToList().FirstOrDefault(p=>p.ProcessName==command.Param!.ToLower());
+                if (process != null)
+                {
+                    process.Kill();
+                    bw.Write($"{process.ProcessName} Killed");
+                }
+                else bw.Write("Your command is not correct");
+                break;
+            case Command.Run:
+                try
+                {
+                    Process.Start($"{command.Param!.ToLower()}");
+                    if (string.IsNullOrEmpty(command.Text)) Console.WriteLine("NUL INPUT");
+                    bw.Write($"{command.Param} Runned");
+                }
+                catch (Exception)
+                {
+                    bw.Write("Your command is not correct");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
